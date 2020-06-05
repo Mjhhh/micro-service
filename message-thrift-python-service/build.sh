2850ae4e038da9738532d209f4245fe7111dcdf0
Mjhhh <523651128@qq.com>
2020-06-05 18:16:18 +0800

    docker化

--- message-thrift-python-service/build.sh
+++ message-thrift-python-service/build.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-docker build -t hub.mooc.com:8080/micro-service/message-service:latest .
-docker push hub.mooc.com:8080/micro-service/message-service:latest
+docker build -t message-service:latest .
